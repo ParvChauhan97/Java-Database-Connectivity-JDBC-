789000c40402839b26d0a581df44950c6cd78024
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-22 09:39:21 +0530

    Add JDBC connection details to TransactionDemo class

--- Java/TransactionDemo.java
+++ Java/TransactionDemo.java
@@ -1,1 +1,10 @@
+import java.sql.*;
 
+public class TransactionDemo {
+    private static final String URL = "jdbc:mysql://localhost:3306/demo_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "secrat";
+
+    public static void main(String[] args) {
+    }
+}
