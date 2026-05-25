d9395770fdafca52daeec6e5f4acf5eb756257e4
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-05-25 09:59:03 +0530

    Define database connection parameters
    Added database connection constants for MySQL.

--- Java/JDBCdemo.java
+++ Java/JDBCdemo.java
@@ -1,5 +1,7 @@
 import java.sql.*;
 
 public class JDBCdemo {
-  
+  private static final String URL = "jdbc:mysql://localhost:3306/demo_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "secrat";
 }
