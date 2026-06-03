511d7afa00a7e89ee511567f2a5d2ba409c3658f
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-03 09:04:48 +0530

    Update in JDBCdemo.java

--- Java/JDBCdemo.java
+++ Java/JDBCdemo.java
@@ -41,7 +41,6 @@ public class JDBCdemo {
             throw new RuntimeException(e);
         }
    }
-
   
     private static void update(Connection conn, int id, String name, String email) {
        String sql = "UPDATE infor SET name = ? , email = ? WHERE id = ? ";
