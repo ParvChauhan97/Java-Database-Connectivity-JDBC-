0fdf79373be4e15cd0230977a5c33807d889a1ff
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-03 09:03:42 +0530

    Add main method to JDBCdemo for database connection

--- Java/JDBCdemo.java
+++ Java/JDBCdemo.java
@@ -5,6 +5,7 @@ public class JDBCdemo {
     private static final String USER = "root";
     private static final String PASSWORD = "secrat";
 
+  
   public static void main(String[] args) {
     try(Connection conn = DriverManager.getConnection(URL , USER, PASSWORD);) {
             System.out.println("Connected to Database.");
