d0d557fbb03d6bc09ee918601c62a396c668370f
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-03 09:04:01 +0530

    Add InsertVal method to insert data into database

--- Java/JDBCdemo.java
+++ Java/JDBCdemo.java
@@ -16,6 +16,7 @@ public class JDBCdemo {
         }
   }
 
+  
   private static void InsertVal(Connection conn, String name, String email) {
     String sql = "INSERT INTO infor(name, email) VALUES (' " +  name + " ' , ' " + email + " ')";
     try(Statement stmt = conn.createStatement()) {
