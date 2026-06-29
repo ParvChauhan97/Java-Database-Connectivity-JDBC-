1f0259598f29a23044220097e6139695e92e09cf
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-29 22:56:50 +0530

    Update in TransactionDemo.java

--- Java/TransactionDemo.java
+++ Java/TransactionDemo.java
@@ -5,7 +5,7 @@ public class TransactionDemo {
     private static final String USER = "root";
     private static final String PASSWORD = "secrat";
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to Database");
             //Turned off Auto Save.
